813a68d1a82af9686f1f046f13cca2f4dacf2f4e
Ray Anthony <16364682+Ray-Anthony@users.noreply.github.com>
2024-07-24 13:55:21 -0500

    Update site settings fragment

--- fragments/SiteSettingsFragment.ts
+++ fragments/SiteSettingsFragment.ts
@@ -2,57 +2,59 @@ import { gql } from "@apollo/client";
 import { MEDIA_ITEM_FRAGMENT } from "./MediaItemFragment";
 
 export const SITE_SETTINGS_FRAGMENT = gql`
-  fragment SiteSettingsFragment on SiteSettings {
-    siteSettings {
-      turnOnAnnouncements
-      customAddressLabel
-      announcements {
-        backgroundColor
-        content
-        ctaType
+  fragment SiteSettingsFragment on ThemeSettings {
+    themeSettings {
+      siteSettings {
+        turnOnAnnouncements
+        customAddressLabel
+        announcements {
+          backgroundColor
+          content
+          ctaType
+          cta {
+            target
+            title
+            url
+          }
+        }
+        email {
+          target
+          title
+          url
+        }
         cta {
           target
           title
           url
         }
-      }
-      email {
-        target
-        title
-        url
-      }
-      cta {
-        target
-        title
-        url
-      }
-      address {
-        city
-        country
-        latitude
-        longitude
-        placeId
-        stateShort
-        streetAddress
-        streetName
-        streetNumber
-        postCode
-        state
-      }
-      customAddressLabel
-      phoneNumber {
-        target
-        title
-        url
-      }
-      logo {
-        ...MediaItemFragment
-      }
-      logoAlt {
-        ...MediaItemFragment
-      }
-      logoWhite {
-        ...MediaItemFragment
+        address {
+          city
+          country
+          latitude
+          longitude
+          placeId
+          stateShort
+          streetAddress
+          streetName
+          streetNumber
+          postCode
+          state
+        }
+        customAddressLabel
+        phoneNumber {
+          target
+          title
+          url
+        }
+        logo {
+          ...MediaItemFragment
+        }
+        logoAlt {
+          ...MediaItemFragment
+        }
+        logoWhite {
+          ...MediaItemFragment
+        }
       }
     }
   }
